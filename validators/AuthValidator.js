b08dd82a27c52b695347a8641e06de835df5c7e0
Pagonoudis <giorgospago23@gmail.com>
2020-02-03 18:38:07 +0200

    update

--- validators/AuthValidator.js
+++ validators/AuthValidator.js
@@ -2,8 +2,8 @@ const { check } = require('express-validator');
 const checkErrors = require('./result');
 
 const login = [
-    check("email").isEmail(),
-    check('password').isLength({min: 3}),
+    check("email").isEmail().withMessage("Invalid value at email field"),
+    check('password').isLength({min: 3}).withMessage("Password must be at least 3 characters"),
     checkErrors
 ];
 
