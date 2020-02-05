53bb463cd82bb88dfbed0268bff2edf9ac971c65
Pagonoudis <giorgospago23@gmail.com>
2020-02-05 20:02:26 +0200

    update

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -8,6 +8,30 @@ route.get("/", AdminAuth, (req, res) => {
         message: "Admin Area"
     });
 });
+route.get("/stats", AdminAuth, async (req, res) => {
+    const categories = await Category.find({}).exec();
+    const labels = [];
+    const counts = [];
+    for (let cat of categories) {
+        const num = await Product.count({category: cat._id});
+        counts.push(num);
+        labels.push(cat.title);
+    }
+
+
+    res.json({
+        success: true,
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    label: "My Categories dataset",
+                    data: counts
+                }
+            ]
+        }
+    });
+});
 route.use("/auth", require("./auth"));
 route.use("/users", AdminAuth, require("./users"));
 route.use("/products", AdminAuth, require("./products"));
